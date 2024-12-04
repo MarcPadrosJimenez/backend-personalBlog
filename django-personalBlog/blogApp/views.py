8075dff12e9cfd6903ab020116dce5c2d7c5001b
Marc Padrós <marcpj14@gmail.com>
2024-12-04 17:21:27 +0100

    update view create_post

--- django-personalBlog/blogApp/views.py
+++ django-personalBlog/blogApp/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .forms import PostForm
 
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/posts/')  # Redirige a una lista de posts
+            post = form.save()
+            return redirect('post_detail', pk=post.pk) # Redirects to post detail
     else:
-        form = PostForm()
+        form = PostForm() 
     return render(request, 'create_post.html', {'form': form})
