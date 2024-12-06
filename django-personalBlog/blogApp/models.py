3e92740be02f495db55a7334d85e0d35b9f814f0
Marc Padrós Jiménez <78456629+Marctrix14@users.noreply.github.com>
2024-12-06 14:07:19 +0100

    correct models

--- django-personalBlog/blogApp/models.py
+++ django-personalBlog/blogApp/models.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from django.db import models
 
 class Section(models.Model):
-    name = models.StringField(required=True)
-    description = models.StringField()
+    name = models.CharField(required=True)
+    description = models.CharField()
 
 class Post(models.Model):
-    content = models.StringField(required=True)
+    content = models.CharField(required=True)
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
-    section = models.ReferenceField(Section, reverse_delete_rule=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
