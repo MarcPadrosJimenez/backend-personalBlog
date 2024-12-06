66a39170984b4da55fa48b3e570a14758ae30705
Marc Padrós Jiménez <78456629+Marctrix14@users.noreply.github.com>
2024-12-06 12:35:19 +0100

    add handle get request list posts

--- django-personalBlog/blogApp/urls.py
+++ django-personalBlog/blogApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('newPost/', views.PostCreateView.as_view()),
+    path('posts/', views.post_list, "post_list"),
 ]
--- django-personalBlog/blogApp/views.py
+++ django-personalBlog/blogApp/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 from django.shortcuts import render
+from django.http import JsonResponse
 
 class PostCreateView(APIView):
     def post(self, request):
@@ -12,5 +13,12 @@ class PostCreateView(APIView):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
+def post_list(request):
+    posts = Post.objects.all()
+    post_data = [
+        {'content': post.content, 'created_at': post.created_at, 'updated_at': post.updated_at} for post in posts
+    ]
+    return JsonResponse(post_data, safe=False)
+
 def index(request):
     return render(request, 'index.html')
